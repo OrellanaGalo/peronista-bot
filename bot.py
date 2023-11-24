d1511a4659d34d6ea22a6b66efb707b11b6807fb
Galo Ignacio Orellana <66918990+OrellanaGalo@users.noreply.github.com>
2023-11-24 11:24:19 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,28 +11,44 @@ access_token_secret = ACCESS_SECRET
 # Set the topic you're interested in
 topic = TOPIC
 
+# List of users to follow
+users_to_follow = ["@GaloOrellana12", "user2", "user3"]  # Add usernames of the
+# users you want to follow
+
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
 
-def repost_tweets():
-    # Search for tweets containing the specified topic
-    tweets = api.search_tweets(q=topic, count=10)
-
-    # Repost each tweet
-    for tweet in tweets:
+def followAndRepostTweets():
+    # Follow specified users
+    for user_to_follow in users_to_follow:
         try:
-            # Avoid retweeting your own tweets to prevent loops
-            if tweet.user.screen_name != BOT_SCREEN_NAME:
-                api.retweet(tweet.id)
-                print(f"Retweeted: {tweet.text}")
-                time.sleep(2)  # To avoid hitting rate limits
+            user = api.get_user(screen_name=user_to_follow)
+            api.create_friendship(user.id)
+            print(f"Followed: {user_to_follow}")
         except tweepy.TweepyException as e:
-            print(f"Error: {e}")
+            print(f"Error following {user_to_follow}: {e}")
+    # Fetch tweets from the specified topic from the users being followed
+    for user_to_follow in users_to_follow:
+        try:
+            tweets = api.user_timeline(screen_name=user_to_follow, count=10, tweet_mode="extended")
+
+            # Repost each tweet related to the specified topic
+            for tweet in tweets:
+                if topic.lower() in tweet.full_text.lower():
+                    # Avoid retweeting tour own tweets to prevent loops
+                    if tweet.user.screen_name != BOT_SCREEN_NAME:
+                        # Retweet the tweet
+                        api.retweet(tweet.id)
+                        print(f"Retweeted from {user_to_follow}: {tweet.full_text}")
+
+                        time.sleep(2)  # To avoid hitting rate limits
+        except tweepy.TweepyException as e:
+            print(f"Error fetching tweets from {user_to_follow}: {e}")
 
 
 # Run the bot every 30 minutes
 while True:
-    repost_tweets()
+    followAndRepostTweets()
     time.sleep(1800)  # 30 minutes
